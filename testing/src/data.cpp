99d1981580aafd2dc7fe1b4bccc29874a838088c
Simon <oehrl@vr.rwth-aachen.de>
2018-04-27 14:46:20 +0200

    Remove functions with no prototypes to fix "no previous prototype for function" warning on clang
    #5

--- testing/src/data.cpp
+++ testing/src/data.cpp
@@ -25,37 +25,5 @@
 #include <vector>
 
 namespace nesci {
-namespace testing {
-
-void unused() { (void)(ANY_MULTIMETER_NAME); }
-
-std::vector<std::string> AnyValueNames() {
-  return std::vector<std::string>{"V_m", "g_e", "g_i"};
-}
-std::string AnyAttribute() { return AnyValueNames()[0]; }
-std::string AnotherAttribute() { return AnyValueNames()[1]; }
-std::string ThirdAttribute() { return AnyValueNames()[2]; }
-
-std::vector<double> AnyAttributesValues(double time) {
-  const double timed_offset = 1.1 * time + 2.2;
-  return std::vector<double>{0.0 + timed_offset, -0.1 + timed_offset,
-                             0.2 + timed_offset, -0.3 + timed_offset,
-                             0.4 + timed_offset, -0.5 + timed_offset};
-}
-std::vector<double> AnotherAttributesValues(double time) {
-  const double timed_offset = 2.2 * time + 3.3;
-  return std::vector<double>{1.0 + timed_offset, -1.1 + timed_offset,
-                             1.2 + timed_offset, -1.3 + timed_offset,
-                             1.4 + timed_offset, -1.5 + timed_offset};
-}
-std::vector<double> ThirdAttributesValues(double time) {
-  const double timed_offset = 3.3 * time + 4.4;
-  return std::vector<double>{-2.01 + timed_offset, 3.12 + timed_offset,
-                             -4.23 + timed_offset, 5.34 + timed_offset,
-                             -6.45 + timed_offset, 7.56 + timed_offset};
-}
-
-std::string AnyMultimeterName() { return "multimeter A"; }
-
-}  // namespace testing
+namespace testing {}  // namespace testing
 }  // namespace nesci
