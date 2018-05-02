da7cd3a4f5cefa92237d6b3081dad397d1190f98
Simon <oehrl@vr.rwth-aachen.de>
2018-05-02 10:03:01 +0200

    Add unused attribute
    #5

--- testing/include/nesci/testing/data.hpp
+++ testing/include/nesci/testing/data.hpp
@@ -31,39 +31,41 @@
 #include "conduit/conduit_node.hpp"
 #include "nesci/testing/conduit_schema.hpp"
 
+#if defined __GNUC__
+#define NESCI_UNUSED __attribute__((unused))
+#else
+#define NESCI_UNUSED
+#endif
+
 namespace nesci {
 namespace testing {
 
 class Data;
 
-#if defined __GNUC__
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-variable"
-#endif
+NESCI_UNUSED static const char* ANY_DEVICE_NAME{"multimeter A"};
+NESCI_UNUSED static const char* NOT_A_DEVICE_NAME{"NOT_A_DEVICE_NAME"};
 
-static const char* ANY_DEVICE_NAME{"multimeter A"};
-static const char* NOT_A_DEVICE_NAME{"NOT_A_DEVICE_NAME"};
+NESCI_UNUSED static const char* ANY_MULTIMETER_NAME{"multimeter A"};
+NESCI_UNUSED static const char* NOT_A_MULTIMETER_NAME{"NOT_A_MULTIMETER_NAME"};
 
-static const char* ANY_MULTIMETER_NAME{"multimeter A"};
-static const char* NOT_A_MULTIMETER_NAME{"NOT_A_MULTIMETER_NAME"};
-
-static const char* ANY_SPIKE_DETECTOR_NAME{"spikes A"};
-static const char* NOT_A_SPIKE_DETECTOR_NAME{"NOT_A_SPIKE_DETECTOR_NAME"};
+NESCI_UNUSED static const char* ANY_SPIKE_DETECTOR_NAME{"spikes A"};
+NESCI_UNUSED static const char* NOT_A_SPIKE_DETECTOR_NAME{
+    "NOT_A_SPIKE_DETECTOR_NAME"};
 
 #define NESCI_TESTING_TRIPLET(type, name, value0, value1, value2, invalid) \
-  static const type ANY_##name{value0};                                    \
-  static const type ANOTHER_##name{value1};                                \
-  static const type THIRD_##name{value2};                                  \
-  static const type NO_##name { invalid }
+  NESCI_UNUSED static const type ANY_##name{value0};                       \
+  NESCI_UNUSED static const type ANOTHER_##name{value1};                                \
+  NESCI_UNUSED static const type THIRD_##name{value2};                                  \
+  NESCI_UNUSED static const type NO_##name { invalid }
 #define NESCI_TESTING_TRIPLET_VECTOR(type, name, value0, value1, value2, \
                                      invalid)                            \
-  static const std::vector<type> ANY_##name##S{value0, value1, value2};  \
+  NESCI_UNUSED static const std::vector<type> ANY_##name##S{value0, value1, value2};  \
   NESCI_TESTING_TRIPLET(type, name, value0, value1, value2, invalid)
 #define NESCI_TESTING_TRIPLET_VECTOR_STRING(name, value0, value1, value2)      \
-  static const std::vector<std::string> ANY_##name##S{value0, value1, value2}; \
+  NESCI_UNUSED static const std::vector<std::string> ANY_##name##S{value0, value1, value2}; \
   NESCI_TESTING_TRIPLET(char*, name, value0, value1, value2, "NO ##name")
 #define NESCI_TESTING_TRIPLET_VECTOR_STRINGIFIED(name)                         \
-  static const std::vector<std::string> ANY_##name##S_STRING{                  \
+  NESCI_UNUSED static const std::vector<std::string> ANY_##name##S_STRING{                  \
       std::to_string(ANY_##name), std::to_string(ANOTHER_##name),              \
       std::to_string(THIRD_##name)};                                           \
   NESCI_TESTING_TRIPLET(char*, name##_STRING, ANY_##name##S_STRING[0].c_str(), \
@@ -92,18 +94,18 @@ inline std::string PathFor(const std::string& device_name,
 }
 
 // clang-format off
-static const std::vector<double> ANY_DATA_VALUES{
+NESCI_UNUSED static const std::vector<double> ANY_DATA_VALUES{
   0.111, 0.112, 0.113,  0.121, 0.122, 0.123,  0.131, 0.132, 0.133,
   0.211, 0.212, 0.213,  0.221, 0.222, 0.223,  0.231, 0.232, 0.233,
   0.311, 0.312, 0.313,  0.321, 0.322, 0.323,  0.331, 0.332, 0.333};
 // clang-format on
-static const double ANY_DATA_VALUE{ANY_DATA_VALUES[0]};
-static const std::vector<double> ANY_DATA_VALUES_FOR_ATTRIBUTES{
+NESCI_UNUSED static const double ANY_DATA_VALUE{ANY_DATA_VALUES[0]};
+NESCI_UNUSED static const std::vector<double> ANY_DATA_VALUES_FOR_ATTRIBUTES{
     ANY_DATA_VALUES[0], ANY_DATA_VALUES[3], ANY_DATA_VALUES[6]};
 
-static const std::size_t TIME_STRIDE{9};
-static const std::size_t ATTRIBUTE_STRIDE{3};
-static const std::size_t ID_STRIDE{1};
+NESCI_UNUSED static const std::size_t TIME_STRIDE{9};
+NESCI_UNUSED static const std::size_t ATTRIBUTE_STRIDE{3};
+NESCI_UNUSED static const std::size_t ID_STRIDE{1};
 
 inline double ValueAt(std::size_t time_id, std::size_t attribute_id,
                       std::size_t neuron_id) {
@@ -157,12 +159,8 @@ inline static const std::string AnyNestDataSchema() {
   return s.str();
 }
 
-static conduit::Node ANY_NEST_DATA{
+NESCI_UNUSED static conduit::Node ANY_NEST_DATA{
     AnyNestDataSchema(), const_cast<double*>(ANY_DATA_VALUES.data()), false};
-
-#if defined __GNUC__
-#pragma GCC diagnostic pop
-#endif
 
 }  // namespace testing
 }  // namespace nesci
