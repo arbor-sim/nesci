8c78e3dbb4a59283eac0de54d3d092a745c29556
Simon <oehrl@vr.rwth-aachen.de>
2018-05-17 16:27:32 +0200

    Use os.environ instead of export
    #8

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -74,8 +74,9 @@ def main(argv):
         execute('mkdir', ['build'])
         os.chdir('build')
         if operating_system == 'Linux':
-            execute('export', ['CC="gcc"'])
-            execute('export', ['CXX="g++"'])
+            os.environ['CC'] = 'gcc'
+            os.environ['CXX'] = 'g++'
+
         execute('conan',
                 ['remote', 'update', 'rwth-vr--bintray',
                  'https://api.bintray.com/conan/rwth-vr/conan'])
