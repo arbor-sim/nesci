671b86ea02b70e07f0668ab45a0ce23a61d8b013
Simon <oehrl@vr.rwth-aachen.de>
2018-05-17 11:36:38 +0200

    Make pipeline fail if subcommand fails
    #8

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import subprocess
 
 valid_stages = ['conan', 'cmake', 'build', 'test', 'deliver']
 valid_os = ['Windows', 'Linux', 'OSX']
@@ -15,6 +16,7 @@ valid_channels = ['develop', 'stable']
 visual_studio_version_year_map = {
     '15': '2017'
 }
+
 
 def get_conan_flags(compiler, compiler_version):
     conan_flags = []
@@ -33,6 +35,7 @@ def get_conan_flags(compiler, compiler_version):
 
     return conan_flags
 
+
 def main(argv):
     if len(argv) != 5:
         print('usage: .gitlab-ci.py stage os compiler compiler_version')
@@ -48,22 +51,27 @@ def main(argv):
         sys.exit(-1)
 
     if not operating_system in valid_os:
-        print('Invalid operating system, possible values: %s' % ', '.join(valid_os))
+        print('Invalid operating system, possible values: %s' %
+              ', '.join(valid_os))
         sys.exit(-1)
 
     if not compiler in valid_compilers[operating_system]:
-        print('Invalid compiler for %s, possible values: %s' % (operating_system, ', '.join(valid_compilers[operating_system])))
+        print('Invalid compiler for %s, possible values: %s' %
+              (operating_system, ', '.join(valid_compilers[operating_system])))
         sys.exit(-1)
 
     if stage == 'conan':
-        os.system('mkdir build')
+        subprocess.check_call('mkdir build')
         os.chdir('build')
         if operating_system == 'Linux':
-            os.system('export CC=gcc')
-            os.system('export CXX=g++')
-        os.system('conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan')
-        os.system('conan user -p %s -r rwth-vr--bintray %s' % (os.environ['CONAN_PASSWORD'], os.environ['CONAN_LOGIN_USERNAME']))
-        os.system('conan install --build=missing %s ..' % ' '.join(get_conan_flags(compiler, compiler_version)))
+            subprocess.check_call('export CC=gcc')
+            subprocess.check_call('export CXX=g++')
+        subprocess.check_call(
+            'conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan')
+        subprocess.check_call('conan user -p %s -r rwth-vr--bintray %s' %
+                              (os.environ['CONAN_PASSWORD'], os.environ['CONAN_LOGIN_USERNAME']))
+        subprocess.check_call('conan install --build=missing %s ..' %
+                              ' '.join(get_conan_flags(compiler, compiler_version)))
 
     elif stage == 'cmake':
         os.chdir('build')
@@ -71,35 +79,41 @@ def main(argv):
         cmake_flags = []
 
         if compiler == 'Visual Studio':
-            cmake_flags.append('-G "Visual Studio %s %s Win64"' % (compiler_version, visual_studio_version_year_map[compiler_version]))
+            cmake_flags.append('-G "Visual Studio %s %s Win64"' %
+                               (compiler_version, visual_studio_version_year_map[compiler_version]))
         else:
             cmake_flags.append('-DCMAKE_BUILD_TYPE=Release')
 
-        os.system('cmake %s ..' % ' '.join(cmake_flags))
+        subprocess.check_call('cmake %s ..' % ' '.join(cmake_flags))
 
     elif stage == 'build':
         build_flags = []
         if compiler == 'Visual Studio':
             build_flags.append('--config Release')
         os.chdir('build')
-        os.system('cmake --build . %s' % ' '.join(build_flags))
+        subprocess.check_call('cmake --build . %s' % ' '.join(build_flags))
 
     elif stage == 'test':
         os.chdir('build')
         if operating_system == 'OSX':
             os.environ['CTEST_OUTPUT_ON_FAILURE'] = '1'
-        os.system('ctest -C Release')
+        subprocess.check_call('ctest -C Release')
 
     elif stage == 'deliver':
         channel = os.environ['channel']
         version = os.environ['version']
         if channel not in valid_channels:
-            print('Invalid channel: %s possible values: %s' % (channel, ', '.join(valid_channels)))
+            print('Invalid channel: %s possible values: %s' %
+                  (channel, ', '.join(valid_channels)))
             sys.exit(-1)
         conan_flags = ' '.join(get_conan_flags(compiler, compiler_version))
-        os.system('conan export-pkg . nesci/%s@RWTH-VR/%s %s -f' % (version, channel, conan_flags))
-        os.system('conan test ./test_package nesci/%s@RWTH-VR/%s %s' % (version, channel, conan_flags))
-        os.system('conan upload nesci/%s@RWTH-VR/%s --all --force -r=rwth-vr--bintray ' % (version, channel))
+        subprocess.check_call('conan export-pkg . nesci/%s@RWTH-VR/%s %s -f' %
+                              (version, channel, conan_flags))
+        subprocess.check_call('conan test ./test_package nesci/%s@RWTH-VR/%s %s' %
+                              (version, channel, conan_flags))
+        subprocess.check_call('conan upload nesci/%s@RWTH-VR/%s --all --force -r=rwth-vr--bintray ' %
+                              (version, channel))
+
 
 if (__name__ == '__main__'):
     main(sys.argv)
