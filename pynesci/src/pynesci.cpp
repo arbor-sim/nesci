db515fc4403d4a70bc9fe18a53cc348256fd9334
Simon <oehrl@vr.rwth-aachen.de>
2018-06-19 15:35:44 +0200

    Exposed node to pynesci module
    #12

--- pynesci/src/pynesci.cpp
+++ pynesci/src/pynesci.cpp
@@ -31,7 +31,10 @@ std::string Greet() { return "G'day!"; }
 
 SUPPRESS_WARNINGS_BEGIN
 // cppcheck-suppress unusedFunction
-BOOST_PYTHON_MODULE(_pynesci) { def("Greet", &Greet); }
+BOOST_PYTHON_MODULE(_pynesci) {
+  def("Greet", &Greet);
+  class_<conduit::Node>("Node");
+}
 SUPPRESS_WARNINGS_END
 
 }  // namespace pynesci
