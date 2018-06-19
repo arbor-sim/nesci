955987a6a0e0d5ee08b16c18a50eb7f0f9111734
Jan Schnathmeier <Jan.Schnathmeier@gmail.com>
2018-06-19 22:30:15 +0200

    include header in source file
    #17

--- consumer/src/spike_detector.cpp
+++ consumer/src/spike_detector.cpp
@@ -18,6 +18,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //------------------------------------------------------------------------------
+
+#include "nesci/consumer/spike_detector.hpp"
+
 namespace nesci {
 namespace consumer {
 
