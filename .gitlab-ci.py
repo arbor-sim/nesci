a778b8a5afc103ec62d57db9745ca07570b10cb1
Jan Müller <j.mueller@vr.rwth-aachen.de>
2018-06-28 08:30:31 +0200

    match gitlab-ci.py to contra's
    #20

--- .gitlab-ci.py
+++ .gitlab-ci.py
@@ -18,30 +18,20 @@ visual_studio_version_year_map = {
 }
 
 
-def execute(command, arguments):
-    call_arguments = [command]
-    call_arguments.extend(arguments)
-    print('\033[92m$ ' + command + ' ' + ' '.join(arguments) + '\033[0m')
-    sys.stdout.flush()
-    return_value = subprocess.call(call_arguments)
-    if return_value != 0:
-        sys.exit(return_value)
-
-
 def get_conan_flags(compiler, compiler_version):
     conan_flags = []
 
-    conan_flags.extend(['-s', 'compiler=%s' % compiler])
-    conan_flags.extend(['-s', 'compiler.version=%s' % compiler_version])
-    conan_flags.extend(['-s', 'arch=x86_64'])
-    conan_flags.extend(['-s', 'build_type=Release'])
+    conan_flags.append('-s compiler="%s"' % compiler)
+    conan_flags.append('-s compiler.version="%s"' % compiler_version)
+    conan_flags.append('-s arch=x86_64')
+    conan_flags.append('-s build_type=Release')
 
     if compiler == 'Visual Studio':
-        conan_flags.extend(['-s', 'compiler.runtime=MT'])
+        conan_flags.append('-s compiler.runtime=MT')
     elif compiler == 'gcc':
-        conan_flags.extend(['-s', 'compiler.libcxx=libstdc++11'])
+        conan_flags.append('-s compiler.libcxx=libstdc++11')
     elif compiler == 'apple-clang':
-        conan_flags.extend(['-s', 'compiler.libcxx=libc++'])
+        conan_flags.append('-s compiler.libcxx=libc++')
 
     return conan_flags
 
@@ -88,38 +78,32 @@ def main(argv):
         execute('mkdir', ['build'])
         os.chdir('build')
 
-        execute('conan',
-                ['remote', 'update', 'rwth-vr--bintray',
-                 'https://api.bintray.com/conan/rwth-vr/conan'])
-        execute('conan', ['user', '-p', os.environ['CONAN_PASSWORD'],
-                          '-r', 'rwth-vr--bintray', os.environ['CONAN_LOGIN_USERNAME']])
-
-        conan_install_flags = ['install', '--build=missing']
-        conan_install_flags.extend(get_conan_flags(compiler, compiler_version))
-        conan_install_flags.append('..')
-        execute('conan', conan_install_flags)
+        os.system(
+            'conan remote update rwth-vr--bintray https://api.bintray.com/conan/rwth-vr/conan')
+        os.system('conan user -p %s -r rwth-vr--bintray %s' %
+                  (os.environ['CONAN_PASSWORD'], os.environ['CONAN_LOGIN_USERNAME']))
+        os.system('conan install --build=missing %s ..' %
+                  ' '.join(get_conan_flags(compiler, compiler_version)))
 
     elif stage == 'cmake':
         os.chdir('build')
 
-        cmake_flags = ['..']
+        cmake_flags = []
 
         if compiler == 'Visual Studio':
-            cmake_flags.extend(['-G', 'Visual Studio %s %s Win64' %
-                                (compiler_version, visual_studio_version_year_map[compiler_version])])
+            cmake_flags.append('-G "Visual Studio %s %s Win64"' %
+                               (compiler_version, visual_studio_version_year_map[compiler_version]))
         else:
             cmake_flags.append('-DCMAKE_BUILD_TYPE=Release')
 
-        execute('cmake', cmake_flags)
+        os.system('cmake %s ..' % ' '.join(cmake_flags))
 
     elif stage == 'build':
-        os.chdir('build')
-
-        cmake_build_flags = ['--build', '.']
+        build_flags = []
         if compiler == 'Visual Studio':
-            cmake_build_flags.extend(['--config', 'Release'])
-
-        execute('cmake', cmake_build_flags)
+            build_flags.append('--config Release')
+        os.chdir('build')
+        os.system('cmake --build . %s' % ' '.join(build_flags))
 
     elif stage == 'test':
         os.chdir('build')
@@ -134,21 +118,13 @@ def main(argv):
             print('Invalid channel: %s possible values: %s' %
                   (channel, ', '.join(valid_channels)))
             sys.exit(-1)
-        conan_flags = get_conan_flags(compiler, compiler_version, '-e CXX=/opt/rh/devtoolset-4/root/usr/bin/c++ -e CC=/opt/rh/devtoolset-4/root/usr/bin/cc')
-
-        conan_export_flags = ['export-pkg', '.',
-                              'nesci/%s@RWTH-VR/%s' % (version, channel), '-f']
-        conan_export_flags.extend(conan_flags)
-        execute('conan', conan_export_flags)
-
-        conan_test_flags = ['test', './test_package', 'nesci/%s@RWTH-VR/%s' %
-                            (version, channel)]
-        conan_test_flags.extend(conan_flags)
-        execute('conan', conan_test_flags)
-
-        conan_upload_flags = ['upload', 'nesci/%s@RWTH-VR/%s' % (version, channel),
-                              '--all', '--force', '-r=rwth-vr--bintray']
-        execute('conan', conan_upload_flags)
+       conan_flags = ' '.join(get_conan_flags(compiler, compiler_version))
+        os.system('conan export-pkg . contra/%s@RWTH-VR/%s %s -f' %
+                  (version, channel, conan_flags))
+        os.system('conan test ./test_package contra/%s@RWTH-VR/%s %s' %
+                  (version, channel, conan_flags))
+        os.system('conan upload contra/%s@RWTH-VR/%s --all --force -r=rwth-vr--bintray ' %
+                  (version, channel))
 
 
 if (__name__ == '__main__'):
