abbf8d4a879d9dd4c1b016bd5ec0a493864ba0f2
Simon Oehrl <simon.oehrl@gmail.com>
2018-07-30 17:32:13 +0200

    Add nesci::testing::ANY_SPIKE_DATA
    #17

--- consumer/tests/test_spike_detector.cpp
+++ consumer/tests/test_spike_detector.cpp
@@ -26,26 +26,18 @@
 #include "nesci/consumer/spike_detector.hpp"
 #include "nesci/testing/data.hpp"
 
-#include "utilities/vector_all_nan_or_empty.hpp"
 #include <iostream>
+#include "utilities/vector_all_nan_or_empty.hpp"
 
-SCENARIO("Test GetNeuronIds",
-	"[nesci][nesci::SpikeDetector]") {
+SCENARIO("Test GetNeuronIds", "[nesci][nesci::SpikeDetector]") {
   GIVEN("a conduit node and a spike detector") {
     nesci::consumer::SpikeDetector spike_detector(
-      nesci::testing::ANY_SPIKE_DETECTOR_NAME);
+        nesci::testing::ANY_SPIKE_DETECTOR_NAME);
 
-    std::string path;
-    path += nesci::testing::ANY_SPIKE_DETECTOR_NAME;
-    path += "/";
-    path += nesci::testing::ANY_TIME_STRING; 
-    conduit::Node node;
-    node[path] = nesci::testing::ANY_IDS;
-
-    spike_detector.SetNode(&node);
+    spike_detector.SetNode(&nesci::testing::ANY_SPIKE_DATA);
 
     std::vector<std::uint64_t> output =
-      spike_detector.GetNeuronIds(nesci::testing::ANY_TIME_STRING);
+        spike_detector.GetNeuronIds(nesci::testing::ANY_TIME_STRING);
 
     for (uint64_t i = 0; i < output.size(); ++i) {
       REQUIRE(output.at(i) == nesci::testing::ANY_IDS.at(i));
--- testing/include/nesci/testing/data.hpp
+++ testing/include/nesci/testing/data.hpp
@@ -170,34 +170,20 @@ inline static const std::string AnyNestDataSchema() {
   return s.str();
 }
 
-inline static const std::string AnyNestSpikeDataSchema() {
-  std::stringstream s;
-  std::size_t offset = 0;
-  const std::size_t datum_size = 8;
-  s << "{\n";
-  s << "  " << conduit_schema::OpenTag(ANY_SPIKE_DETECTOR_NAME);
-  for (auto time : ANY_TIMES) {
-    s << "    " << conduit_schema::OpenTag(time);
-    for (auto id : ANY_IDS) {
-      s << "        " << conduit_schema::OpenTag(id);
-      s << "          "
-        << conduit_schema::DoubleData((offset++) * datum_size);
-      s << "        " << conduit_schema::CloseTagNext();
-    }
-    conduit_schema::RemoveNextIndicator(&s);
-    s << "    " << conduit_schema::CloseTagNext();
-  }
-  conduit_schema::RemoveNextIndicator(&s);
-  s << "  " << conduit_schema::CloseTag();
-  s << "}";
-  return s.str();
+inline static const conduit::Node AnyNestSpikeData() {
+  std::string path;
+  path += nesci::testing::ANY_SPIKE_DETECTOR_NAME;
+  path += "/";
+  path += nesci::testing::ANY_TIME_STRING;
+  conduit::Node node;
+  node[path] = nesci::testing::ANY_IDS;
+  return node;
 }
 
 NESCI_UNUSED static conduit::Node ANY_NEST_DATA{
     AnyNestDataSchema(), const_cast<double*>(ANY_DATA_VALUES.data()), false};
 
-NESCI_UNUSED static conduit::Node ANY_SPIKE_DATA{
-  AnyNestSpikeDataSchema(), const_cast<double*>(ANY_DATA_VALUES.data()), false };
+NESCI_UNUSED static conduit::Node ANY_SPIKE_DATA = AnyNestSpikeData();
 
 }  // namespace testing
 }  // namespace nesci
